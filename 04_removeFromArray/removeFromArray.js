268517ea9afbd23c3696fa27eeddf0051e1b2f52
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 19:22:53 -0400

    Add solution removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,16 @@
-let array = []
-let index;
-
 const removeFromArray = function(elements, ...args) {
-    array = elements
-    for (let i of args) {
-        index = array.indexOf(i)
-        array.splice(index, 1);
-    }
-    return array;
+  return elements.filter(i => !args.includes(i))
 }
 
 
-removeFromArray([1, 2, 3, 4], 3)
-removeFromArray([1, 2, 3, 4], 3, 2)
-
-
-
-
-
-
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
