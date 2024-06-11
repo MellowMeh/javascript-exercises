4f50f04faf6f3b8796047c863f0951a1eca03900
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 12:48:35 -0400

    Add solution for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,18 +1,22 @@
-let string;
+let string = "";
 let letterSelected = "";
-let stringReversed = "";
 
 const reverseString = function(string) {
+    let stringReversed = "";
     for (let i = string.length; i > -1; i--)
         if (string.length > stringReversed.length) {
             letterSelected = string.charAt(i);
             stringReversed += letterSelected;
         }
-        return stringReversed;
+    return stringReversed;
 };
 
 
+
 reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 
 
 // Do not edit below this line
