5c4e09df8be5f1f4c0028a147423bc3f1bc15727
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 18:42:35 -0400

    Add solution remove mulitple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,14 +3,16 @@ let index;
 
 const removeFromArray = function(elements, ...args) {
     array = elements
-    index = array.indexOf(...args)
-    array.splice(index, 1);
+    for (let i of args) {
+        index = array.indexOf(i)
+        array.splice(index, 1);
+    }
     return array;
-};
+}
 
 
 removeFromArray([1, 2, 3, 4], 3)
-
+removeFromArray([1, 2, 3, 4], 3, 2)
 
 
 
