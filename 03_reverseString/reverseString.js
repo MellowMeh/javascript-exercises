067204bf5becd2cc27760110e9fcf7074d76bc2c
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 10:51:56 -0400

    Add pseudocode

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -4,3 +4,10 @@ const reverseString = function() {
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//GET function accepting string as argument 
+    //convert string to array
+    //reverse array
+    //convert array to string
+    //RETURN new string
