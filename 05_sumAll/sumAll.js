9bdb0fdc494b4e7b808be4fa0dd1f22beddfac89
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 19:39:10 -0400

    Add convert arguments to array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let sum = 0;
+    let numberArray = Array.from(args)
+    return numberArray;
+}
 
-};
+
+
+sumAll(1, 4);
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+//find smallest value
+//repeat a loop incrementing i until i = largest value
+    //for each loop add that i to the sum
