ce7e6837e68b690548cb79a58193764c0863fb19
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-11 17:55:26 -0400

    Add removes a single value solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+let array = []
+let index;
+
+const removeFromArray = function(elements, ...args) {
+    array = elements
+    index = array.indexOf(...args)
+    array.splice(index, 1);
+    return array;
 };
+
+
+removeFromArray([1, 2, 3, 4], 3)
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
