6cc9d7b0dc836ab36a8d1ab85f3230e79bfb5717
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-12 09:59:05 -0400

    Add solution round to decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let num = (degreesFahrenheit -32) * 5/9; //F to C formula
+  let n = num.toFixed(1); //rounds first place after decimal, but converts to string
+  let result = Number(n); //convert string back to number
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let num = degreesCelsius * 9/5 + 32; // C to F formula
+  let n = num.toFixed(1); //rounds first place after decimal, but converts to string
+  let result = Number(n); //convert string back to number
+  return result;
+
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+
+
+
+//x°C ≘ (x × 9/5 + 32)°F	
+//x°F ≘ (x − 32) × 5/9°C
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
