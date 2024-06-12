c36942bc0df439b66cbf2072501577c13cb1148e
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-12 16:51:43 -0400

    Fix string method indentation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,17 @@
 const palindromes = function (string) {
     acceptedValues = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    cleanString = string.toLowerCase().split("").filter(item => acceptedValues.includes(item)).join("");
-    reverseString = string.toLowerCase().split("").filter(item => acceptedValues.includes(item)).reverse().join("");
+    cleanString = 
+        string.toLowerCase()
+        .split("")
+        .filter(item => acceptedValues
+        .includes(item))
+        .join("");
+    reverseString = 
+        string.toLowerCase()
+        .split("")
+        .filter(item => acceptedValues.includes(item))
+        .reverse()
+        .join("");
     if (cleanString === reverseString) {
         return true;
     } else {
