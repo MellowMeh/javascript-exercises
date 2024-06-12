1b272f3b91df3d2c75603efac3d60b6b6bb44220
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-12 12:05:14 -0400

    Add palindrome no punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    reversedString = string.split("").reverse().join("")
+    if (reversedString === string) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes('racecar');
+palindromes('racecar!');
+
 
 // Do not edit below this line
 module.exports = palindromes;
