319d7d4cd40b5bbcf248d4417588df8044cfcb5a
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-12 09:28:42 -0400

    Add conditionals for century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     let result = false;
-    if (year % 4 === 0 || year % 400 === 0) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
         result = true;
     }
     return result;
@@ -9,7 +9,9 @@ const leapYears = function(year) {
 leapYears(1996);
 leapYears(1997);
 leapYears(34992);
-
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 
 // Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
