62ac6bed3fccad8952e983c35b9be3765393f16e
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-13 11:00:25 -0400

    Add convert string inputs to number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    //convert string inputs to number
+    selectedIndex = Number.parseInt(input);
+    return selectedIndex;
 
-};
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+//construct array
+//select number to find sum of...in this case 8
+    //numOne = array[num - 1]
+    //numTwo = array[num -2]
+    //result = numOne + numTwo
