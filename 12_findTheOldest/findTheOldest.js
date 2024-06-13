38629ce58a3132d119ff519fbc7219f7b522dae5
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-06-13 18:36:40 -0400

    Add reduce method solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear();
+    } 
+    return death - birth;
 };
+
+const findTheOldest = function (people) {
+    return people.reduce((personOne, personTwo) => {
+        const personOneAge = getAge(personOne.yearOfBirth, personOne.yearOfDeath);
+        const personTwoAge = getAge(personTwo.yearOfBirth, personTwo.yearOfDeath);
+        return personOneAge < personTwoAge ? personTwo: personOne;
+    }
+)};
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+//reduce each element with function age => yearOfDeath-yearOfBirth
